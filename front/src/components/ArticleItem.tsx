ce84e1e58a31cac8bcb41e883343221fc0faf20e
kitune-chan-250r <nekoo.hisui@gmail.com>
2025-08-02 17:48:30 +0000

    リンクを追加

--- front/src/components/ArticleItem.tsx
+++ front/src/components/ArticleItem.tsx
@@ -1,15 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import type { ShelfData } from "../types";
+import { useNavigate } from "react-router";
 
 interface ArticleItemProps {
   key: string;
   data: ShelfData;
 }
 export const ArticleItem = ({ key, data }: ArticleItemProps) => {
+  const navigate = useNavigate();
   return (
     <Box
       key={key}
       // elevation={3}
+      onClick={() => navigate(`/article/${data.filename}`)}
       sx={{
         mt: 1.5,
         mb: 1.5,
@@ -18,6 +21,7 @@ export const ArticleItem = ({ key, data }: ArticleItemProps) => {
         maxWidth: 900,
         opacity: 0.8,
         border: "solid 1px #8d7092ff",
+        cursor: "pointer",
       }}>
       <Box
         sx={{
